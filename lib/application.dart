6affc18ad8fab8d2cf724268afaa9de2adb1ee41
admin <admin@example.com>
2023-04-24 04:56:49 +0100

    Testcase for checking widget render is successfull

---
+++ lib/application.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
