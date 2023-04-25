41a64a7252771c849aaa17898d66a35fdb75d179
admin <admin@example.com>
2023-04-25 06:55:52 +0100

    adding support for get_it

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:edu_test_demo/application.dart';
+import 'package:edu_test_demo/setup_locator.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  setupLocator();
   runApp(const Application());
 }
